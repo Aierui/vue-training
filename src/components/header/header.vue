8ab1cc303b0bafe68a779bd2179ad46510111011
Astonep <imland@outlook.com>
2016-12-04 02:33:25 +0800

    training

---
+++ src/components/header/header.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+wo shi header  
+</div>
+  
+</template>
+
+<script type="text/ecmascript-6">
+export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+ 
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import goods from 'components/goods/goods'
+import seller from 'components/seller/seller'
+import ratings from 'components/ratings/ratings'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/goods', component: goods },
+  { path: '/seller', component: seller },
+  { path: '/ratings', component: ratings }
+]
+
+const router = new VueRouter({
+  routes: routes
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router: router,
+  render: h => h(App)
+})
